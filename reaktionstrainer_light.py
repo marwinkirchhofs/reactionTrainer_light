9efc1ed477a437273b5a7152f181ae4471795833
Marwin Kirchhofs <m.kirchhofs@gmx.de>
2020-02-23 18:35:10 +0100

    modifiying waiting interval added

--- reaktionstrainer_light.py
+++ reaktionstrainer_light.py
@@ -47,6 +47,7 @@ class StoppableThread(Thread):
 
 
 def fct_runColors(arg_label):
+
 	sleepOffset = sleepRange[0]
 	sleepDuration = sleepRange[1] - sleepRange[0]
 
@@ -55,7 +56,7 @@ def fct_runColors(arg_label):
 	newColor = numColor
 	arg_label.configure(bg=colors[numColor])
 	print("new color = " + colors[numColor])
-	timeSleep = sleepOffset + floor( sleepDuration*random() )
+	timeSleep = sleepOffset + ( sleepDuration*random() )
 	sleep(timeSleep)
 	while True:
 		# close thread if stop signal is set
@@ -74,7 +75,7 @@ def fct_runColors(arg_label):
 			# set new color
 			arg_label.configure(bg=colors[numColor])
 
-			timeSleep = sleepOffset + floor( sleepDuration*random() )
+			timeSleep = sleepOffset + (sleepDuration*random() )
 			sleep(timeSleep)
 			
 
@@ -84,7 +85,20 @@ def btCmd_start(event=0):
 
 def btCmd_stop(event=0):
 	print("stop button clicked")
+	print("max: " + str_maxWait + ", min: " + str_minWait)
 
+def updateWait(str_minWait, str_maxWait):
+	print("min: " + str_minWait.get() + "max: " + str_maxWait.get())
+	# update time values if valid
+	try:
+		minWait = int(str_minWait.get())
+		maxWait = int(str_maxWait.get())
+		if not maxWait < minWait:
+			sleepRange[0] = minWait
+			sleepRange[1] = maxWait
+	except:
+		pass
+	
  
 ################ PROGRAM START ################
 
@@ -107,18 +121,39 @@ bt_quit = Button(mainWindow, text="Quit", command=mainWindow.quit)
 # create empty label as colour area
 label_colourArea = Label(mainWindow, text="", bg="red")
 
+# labels and entry fields for wait values
+label_waitTime = Label(mainWindow, text="Anzeigezeit:")
+label_minWait = Label(mainWindow, text="min.")
+label_maxWait = Label(mainWindow, text="max.")
+str_minWait = StringVar(mainWindow)
+str_maxWait = StringVar(mainWindow)
+str_minWait.trace_add("write", lambda name, index, mode, str_minWait=str_minWait, str_maxWait=str_maxWait: updateWait(str_minWait, str_maxWait))
+str_maxWait.trace_add("write", lambda name, index, mode, str_minWait=str_minWait, str_maxWait=str_maxWait: updateWait(str_minWait, str_maxWait))
+entry_minWait = Entry(mainWindow, textvariable=str_minWait)
+entry_minWait.insert(10,"4")
+entry_maxWait = Entry(mainWindow, textvariable=str_maxWait)
+entry_maxWait.insert(10,"7")
+
 # add components to mainWindow
-bt_start.grid(row=1, column=0)
-bt_stop.grid(row=1, column=1)
-bt_quit.grid(row=1, column=2)
-label_colourArea.grid(row=0, column=0, sticky=W+E+N+S, columnspan=3)
+label_colourArea.grid(row=0, column=0, sticky=W+E+N+S, columnspan=5)
+label_waitTime.grid(row=1, column=0)
+label_minWait.grid(row=1, column=1)
+entry_minWait.grid(row=1, column=2)
+label_maxWait.grid(row=1, column=3)
+entry_maxWait.grid(row=1, column=4)
+bt_start.grid(row=2, column=0, columnspan=2)
+bt_stop.grid(row=2, column=2, columnspan=2)
+bt_quit.grid(row=2, column=4)
 
 # set up row and column weights
 mainWindow.grid_rowconfigure(0, weight=4)
 mainWindow.grid_rowconfigure(1, weight=0)
+mainWindow.grid_rowconfigure(2, weight=0)
 mainWindow.grid_columnconfigure(0, weight=1)
 mainWindow.grid_columnconfigure(1, weight=1)
 mainWindow.grid_columnconfigure(2, weight=1)
+mainWindow.grid_columnconfigure(4, weight=1)
+mainWindow.grid_columnconfigure(3, weight=1)
 
 
 ##############################
